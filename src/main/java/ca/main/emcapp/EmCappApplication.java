0c62288832159564afe6d23f416c6846ac0f2c81
Khoa <khoalittlesaigon@hotmail.com>
2018-01-11 18:38:59 -0600

    Initial Commit

---
+++ src/main/java/ca/main/emcapp/EmCappApplication.java
@@ -0,0 +1,12 @@
+package ca.main.emcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmCappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmCappApplication.class, args);
+	}
+}
